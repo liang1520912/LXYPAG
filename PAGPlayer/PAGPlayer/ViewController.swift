00f741db3931df62a0ba199f1588a3e337157e62
liang1520912 <422892962@qq.com>
2023-10-08 10:39:57 +0800

    no message

--- PAGPlayer/PAGPlayer/ViewController.swift
+++ PAGPlayer/PAGPlayer/ViewController.swift
@@ -10,30 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     lazy var pagView :LXYPAGView = {
         let pagView = LXYPAGView()
-       
+        
         return pagView
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            let config = LXYPAGConfig()
-            if let path = Bundle.main.path(forResource: "like", ofType: ".pag") {
-                config.resourceStr = path
-            }
-
+        let config = LXYPAGConfig()
+        if let path = Bundle.main.path(forResource: "like", ofType: ".pag") {
+            config.resourceStr = path
+        }
         
-            config.loop = 0
+        config.loop = 2
         pagView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-    
-            self.pagView.playAnim(config)
+        
+        self.pagView.playAnim(config)
         
         view.addSubview(pagView)
         
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-       
+        
     }
 }
 
